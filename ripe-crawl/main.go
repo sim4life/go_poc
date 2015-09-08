67314c24afa57694834c7e673194c7372f16782e
Shah Ismail <sim4life@yahoo.com>
2015-09-08 19:21:21 +0100

    Added comments

--- ripe-crawl/main.go
+++ ripe-crawl/main.go
@@ -19,26 +19,40 @@ import (
 	iconv "github.com/djimenez/iconv-go"
 )
 
-type FruitItem struct {
-	Title       string  `json:"title"`
-	UnitPrice   float32 `json:"unit_price"`
-	Size        float32 `json:"size"`
-	Description string  `json:"description"`
-	Uri         string  `json:"-"` //ignored on marshalling
+// Custom type defined to make sure JSON output contains precision of 2 decimal points
+type Number float32
+
+func (n Number) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("%.2f", n)), nil
 }
 
-// fatal if there is an error
+// Basic struct to be used for channel sharing and JSON Marshaling
+type FruitItem struct {
+	Title       string `json:"title"`
+	Size        string `json:"size"`
+	UnitPrice   Number `json:"unit_price"`
+	Description string `json:"description"`
+	DetailsUri  string `json:"-"` //ignored on marshaling
+}
+
+// Fatal if there is an error
 func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func extractFloat(value string) float32 {
+/**
+ * This function parses a value string parameter and returns Number value
+ * embedded within the string. It returns nil if it doesn't find any
+ * Number value in the value string.
+ * Example: "some4.56more" would return 4.56
+ */
+func extractFloat32(value string) Number {
 	var sc scanner.Scanner
 	var tok rune
 	var valFloat64 float64
-	var valFloat32 float32
+	var valFloat32 Number
 	var err error
 	var isFound bool
 
@@ -54,62 +68,65 @@ func extractFloat(value string) float32 {
 				isFound = true
 				break
 			}
-			// checkErr(err)
 		}
 	}
 
 	if isFound {
-		valFloat32 = float32(valFloat64)
+		valFloat32 = Number(valFloat64)
 	}
 
 	return valFloat32
 }
 
+/**
+ * This function parses and returns the uri associated with the HTML anchor
+ * <a href="http://www..."...> tag
+ * This function assumes that 'href' attribute contains absolute url.
+ * It returns "" empty string if it can't find href attribute from the
+ * goquery.Selection parameter.
+ */
 func getUri(sel *goquery.Selection) string {
 	if sel != nil {
 		str, exists := sel.Attr("href")
-		// fmt.Println("str is: ", str)
 		if exists {
 			u, err := url.Parse(str)
-			// fmt.Println("u is: ", u)
 			checkErr(err)
 			return u.String()
 		}
 	}
-
 	return ""
 }
 
-func productScrape(client *http.Client, uri string, fruitInQueue chan *FruitItem) {
+/**
+ * This function scrapes the fruit item title and unit price from the downloaded
+ * HTML document. First, it downloads the HTML doc from the given URI parameter.
+ * Then, it scrapes the products' title, unit price and details URI info from the
+ * downloaded HTML doc. Later on, it creates fruit items with these partial values,
+ * and then it puts these fruit item objects into fruitInQueue channel.
+ */
+func fruitInitScrape(client *http.Client, uri string, fruitInQueue chan *FruitItem) {
 
-	// Load the URL
-	// res, err := http.Get(uri)
+	var price Number
+	var addPrice Number
+	var iter int
+
+	// Load the URI
 	res, err := client.Get(uri)
 	checkErr(err)
 
 	defer res.Body.Close()
 
-	// body, err := ioutil.ReadAll(res.Body)
-	// checkErr(err)
-	// fmt.Println("before body is:\n", string(body))
-
-	// Convert the designated charset HTML to utf-8 encoded HTML.
-	// `charset` being one of the charsets known by the iconv package.
+	// Convert the "windows-1252" charset of the downloaded HTML to
+	// utf-8 encoded HTML.
 	utfBody, err := iconv.NewReader(res.Body, "windows-1252", "utf-8")
 	checkErr(err)
 
 	// use utfBody using goquery
 	doc, err := goquery.NewDocumentFromReader(utfBody)
-	// doc, err := goquery.NewDocument(uri)
-	// doc, err := goquery.NewDocumentFromResponse(res)
 	checkErr(err)
-	var price float32
-	var addPrice float32
-	var iter int
 
-	fmt.Println("doc is:\n", doc)
-	// fmt.Println("doc.Contents is:\n", doc.Contents())
 	fmt.Println("about to find stuff\n")
+	// Find required info within the document
 	doc.Find("ul.productLister li").Each(func(i int, s *goquery.Selection) {
 		product := s.Find(".productInner h3 a")
 		title := strings.TrimSpace(product.Text())
@@ -121,147 +138,143 @@ func productScrape(client *http.Client, uri string, fruitInQueue chan *FruitItem
 		addTitle := strings.TrimSpace(addProduct.Text())
 		addPriceStr := strings.TrimSpace(s.Find(".crossSellInner p.pricePerUnit").Text())
 
-		price = extractFloat(priceStr)
+		// Parsing float32 value
+		price = extractFloat32(priceStr)
 
-		// fmt.Printf("Title %d: %s - %.2f\n", i, title, price)
-		// fmt.Printf("URI: %s\n", prodUri)
 		iter++
-		fruitItem := &FruitItem{Title: title, UnitPrice: price, Size: 0, Description: "", Uri: prodUri}
-		// fmt.Println("Found stuff=============>\n")
+		// Creating fruit item with partial values
+		fruitItem := &FruitItem{Title: title, UnitPrice: price, Size: "0kb", Description: "", DetailsUri: prodUri}
+		// Pretty-printing the progress of in channel processing
 		fmt.Print("Found stuff")
 		for i := 0; i < iter; i++ {
 			fmt.Print("=")
 		}
 		fmt.Println(">\n")
 
-		// go func() {
+		// Putting partially formed fruitItem on to fruitInQueue channel
 		fruitInQueue <- fruitItem
-		// }()
+
+		// These additional fruit items are the cross selling product items
 		if len(addTitle) > 0 {
-			addPrice = extractFloat(addPriceStr)
+			addPrice = extractFloat32(addPriceStr)
 			addProdUri := getUri(addProduct)
-			// fmt.Printf("Additional Title %d: %s - %.2f\n", i, addTitle, addPrice)
-			// fmt.Printf("Additional URI: %s\n", addProdUri)
 
 			iter++
-			fruitItem := &FruitItem{Title: addTitle, UnitPrice: addPrice, Size: 0, Description: "", Uri: addProdUri}
-			// fmt.Println("Found stuff============>\n")
+			// Creating fruit item with partial values
+			fruitItem := &FruitItem{Title: addTitle, UnitPrice: addPrice, Size: "0kb", Description: "", DetailsUri: addProdUri}
+			// Pretty-printing the progress of in channel processing
 			fmt.Print("Found stuff")
 			for i := 0; i < iter; i++ {
 				fmt.Print("=")
 			}
 			fmt.Println(">\n")
 
-			// go func() {
+			// Putting partially formed fruitItem on to fruitInQueue channel
 			fruitInQueue <- fruitItem
-			// }()
-
 		}
 	})
 
+	fmt.Println("\nfinished finding stuff ... closing channel\n")
+	// Closing the In channel as it is not needed
 	close(fruitInQueue)
-	fmt.Println("\nfinished finding stuff\n")
 
 }
 
-func retrieveSizeDesc(client *http.Client, fruitInQueue, fruitOutQueue chan *FruitItem) {
+/**
+ * This function retrieves the size (KB) of the downloaed HTML document (without assets)
+ * and scrapes the description of the fruit item from downloaded HTML doc. First, it
+ * consumes the fruit item struct from fruitInQueue channel. Then, it downloads the HTML
+ * document to find and save size and description in the same fruit item struct. Lastly,
+ * it puts this completely formed fruit item struct in the fruitOutQueue channel
+ */
+func fruitFinishScrape(client *http.Client, fruitInQueue, fruitOutQueue chan *FruitItem) {
 	var iter int
+	// Consuming fruitItem from fruitInQueue channel
 	for fruitItem := range fruitInQueue {
-		// fmt.Printf("In is: %+v\n", fruitItem)
+
 		fmt.Println("about to find MORE stuff\n")
 		iter++
-		res, err := client.Get(fruitItem.Uri)
+
+		res, err := client.Get(fruitItem.DetailsUri)
 		checkErr(err)
 
 		defer res.Body.Close()
+		// Reading to calculate the size of HTML document in kb
 		body, err := ioutil.ReadAll(res.Body)
 		checkErr(err)
-		// fmt.Println("before body is:\n", len([]byte(string(body))))
-		// Converting bytes into kb
+
+		// Converting bytes into KB
 		size := float32(len(body)) / float32(1024)
-		// Restore the io.ReadCloser to its original state to be re-read
+
+		// Restoring the io.ReadCloser to its original state to be re-read
 		res.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-		// fmt.Printf("len(body) is: %.2f\n", size)
-		// doc, err := goquery.NewDocument(fruitItem.uri)
+		// Querying from the restored http.Response
 		doc, err := goquery.NewDocumentFromResponse(res)
 		checkErr(err)
-		// fmt.Println("doc MORE is:\n", doc)
 
-		// fmt.Println("HTML.size is:\n", res.Body)
-
-		// fmt.Println("doc.Contents is:\n", doc.Contents())
-
+		// Looking for fruit item description within newly downloaded HTML document
 		doc.Find("#information").Each(func(i int, s *goquery.Selection) {
 			desc := strings.TrimSpace(s.Find(".productText p").First().Text())
 
-			// fmt.Println("prod desc is: ", desc)
-
 			fruitItem.Description = desc
-			fruitItem.Size = size
+			fruitItem.Size = strconv.FormatFloat(float64(size), 'f', 2, 32) + "kb"
 
-			// fmt.Printf("In is: %+v\n", fruitItem)
-			// fmt.Printf("len(OutQueue) is: %d\n", len(fruitOutQueue))
+			// Pretty-printing the progress of out channel processing
 			fmt.Print("<")
-			// fmt.Println("<============FOUND stuff\n")
 			for i := 0; i < iter; i++ {
 				fmt.Print("=")
 			}
 			fmt.Println("FOUND more stuff\n")
-			// go func(fruitItem *FruitItem) {
+
+			// Putting completely formed fruitItem on to fruitOutQueue channel
 			fruitOutQueue <- fruitItem
-			// }(fruitItem)
 
 		})
 	}
-	fmt.Println("\nfinished finding MORE stuff\n")
+	fmt.Println("\nfinished finding MORE stuff ... closing channel\n")
+	// Closing the Out channel as it is not needed
 	close(fruitOutQueue)
 }
 
-func buildJSON(fruitOutQueue chan *FruitItem) {
-	fruitList := make([]*FruitItem, 0)
-	totalPrice := float32(0)
+/**
+ * This function returns a json []byte from the fruit items. First, it
+ * consumes fruit items from fruitOutQueue channel. Then it appends it to
+ * fruitsList and finds the cumulative unit price of all the fruit items.
+ * Later on, it builds the JSON object and returns it.
+ */
+func getFruitsJSON(fruitOutQueue chan *FruitItem) []byte {
+	fruitsList := make([]*FruitItem, 0)
+	totalPrice := Number(0)
+
 	var iter int
 	for fruitItem := range fruitOutQueue {
 		iter++
-		// fmt.Println("Thanks <---> stuff\n")
+		// Pretty-printing the progress of out channel processing
 		fmt.Print("Thanks <")
 		for i := 0; i < iter; i++ {
 			fmt.Print("-")
 		}
 		fmt.Println("> stuff\n")
 
-		// fmt.Printf("OutItem is: %+v\n", fruitItem)
-		fruitList = append(fruitList, fruitItem)
+		fruitsList = append(fruitsList, fruitItem)
 		totalPrice += fruitItem.UnitPrice
 	}
-	/*
-		for i, fruitItem := range fruitList {
-			fmt.Printf("ListItem[%d] is: %+v\n", i, fruitItem)
-			/*
-				fruitJSON, err := json.Marshal(*fruitItem)
-				checkErr(err)
-				fmt.Println("JSON is: ", string(fruitJSON))
 
-		}
-	*/
-	type FruitsTotal struct {
-		Results    []*FruitItem `json:"results"`
-		TotalPrice float32      `json:"total"`
-	}
 	fmt.Println("Total Unit Price is: ", totalPrice)
-	// fruitsJSON, err := json.Marshal(fruitList)
-	fruitsTotal := &FruitsTotal{Results: fruitList, TotalPrice: totalPrice}
-	fruitsJSON, err := json.MarshalIndent(fruitsTotal, "", "    ")
+
+	// A temporary struct is defined to generate desired JSON
+	fruitsJSON, err := json.MarshalIndent(struct {
+		Results    []*FruitItem `json:"results"`
+		TotalPrice Number       `json:"total"`
+	}{Results: fruitsList, TotalPrice: totalPrice},
+		"", "   ")
 	checkErr(err)
-	// finalJSON := []byte(`{"results":`)
-	// finalJSON = append(finalJSON, fruitsJSON...)
-	// endJSON := []byte(`,"total":`)
-	fmt.Println("JSON is: ", string(fruitsJSON))
+
+	return fruitsJSON
 
 }
 
 func main() {
-	// ExampleScrape()
 
 	uri := "http://www.sainsburys.co.uk/webapp/wcs/stores/servlet" +
 		"/CategoryDisplay?listView=true&orderBy=FAVOURITES_FIRST&" +
@@ -273,8 +286,6 @@ func main() {
 		"orderBy=FAVOURITES_FIRST&searchTerm=&beginIndex=0&" +
 		"hideFilters=true"
 
-		//uri2 := "http://www.sainsburys.co.uk/webapp/wcs/stores/servlet/CategoryDisplay?listView=true&orderBy=FAVOURITES_FIRST&parent_category_rn=12518&top_category=12518&langId=44&beginIndex=0&pageSize=20&catalogId=10137&searchTerm=&categoryId=185749&listId=&storeId=10151&promotionId=#langId=44&storeId=10151&catalogId=10137&categoryId=185749&parent_category_rn=12518&top_category=12518&pageSize=20&orderBy=FAVOURITES_FIRST&searchTerm=&beginIndex=0&hideFilters=true"
-
 	options := cookiejar.Options{
 		PublicSuffixList: publicsuffix.List,
 	}
@@ -283,11 +294,12 @@ func main() {
 	client := http.Client{Jar: jar}
 
 	fmt.Println("the uri is: ", uri)
-	//fmt.Println("the uri2 is:", uri2)
+
 	fruitInQueue := make(chan *FruitItem, 2)
 	fruitOutQueue := make(chan *FruitItem)
-	go productScrape(&client, uri, fruitInQueue)
-	go retrieveSizeDesc(&client, fruitInQueue, fruitOutQueue)
-	buildJSON(fruitOutQueue)
+	go fruitInitScrape(&client, uri, fruitInQueue)
+	go fruitFinishScrape(&client, fruitInQueue, fruitOutQueue)
+	fruitsJSON := getFruitsJSON(fruitOutQueue)
+	fmt.Println("JSON is: ", string(fruitsJSON))
 
 }
